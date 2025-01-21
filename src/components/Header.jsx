86231d0b66fb9344931e744129d4c83ced4e8973
Lou Thomas <louthomas35.lt@gmail.com>
2025-01-21 11:17:12 +0100

    Fixing menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import Logo from '../assets/logo.jpg';
 import '../styles/components/header.css';
-/*import Account from '../assets/homepage/icon_client.png'
-import Cart from '../assets/homepage/icon_cart.png'*/
 
 const Header = () => {
-  return (
-    <div>
-      <img className="logo" src={Logo} alt="logo du site" />
-      {/*<img className="account" src={Account} alt="icon de compte" />
-      <img className="cart" src={Cart} alt="icon de panier" />*/}
-      <section className='menu'>
-        <nav>
-          <li>
-            <a href="/">Accueil</a>
-            <a href="/boutique">Boutique en ligne</a>
-            <a href="/contact">Contact</a>
-          </li>
-        </nav>
-      </section>
-    </div>
-  );
+    return (
+        <header>
+            <div>
+                <img className="logo" src={Logo} alt="logo du site"/>
+            </div>
+            <nav className='menu'>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="/boutique">Boutique en ligne</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/connexion" className='connexion'>Connexion</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
