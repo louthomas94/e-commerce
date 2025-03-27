87015e85ede388314ed83f5cc528b721832e8047
adyanjeffali <adyan.jeff@gmail.com>
2025-03-27 23:10:32 +0100

    Amélioration des pages Contact et Connexion avec nouveau style CSS

--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -3,16 +3,21 @@ import "../styles/pages/connexion.css";
 
 const Connexion = () => {
   return (
-    <section className="connexion">
-      <h1 className="Tconnexion">Connexion à son compte</h1>
-      <form className="Fconnexion" action="" method="POST">
-        <label>Nom d'utilisateur</label>
-        <input type="text" name="nom_user" required />
-        <label>Mot de passe</label>
-        <input type="password" name="mdp" required />
-        <input type="submit" value="Se connecter" name="boutton-valider" />
-      </form>
-    </section>
+    <div className="connexion-container">
+      <div className="connexion-image"></div> {/* Section Image */}
+      <div className="connexion-form">       {/* Section Formulaire */}
+        <h1 className="Tconnexion">Connexion à son compte</h1>
+        <form className="Fconnexion" action="" method="POST">
+          <label>Nom d'utilisateur</label>
+          <input type="text" name="nom_user" required />
+
+          <label>Mot de passe</label>
+          <input type="password" name="mdp" required />
+
+          <input type="submit" value="Se connecter" name="boutton-valider" />
+        </form>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
 import '../styles/pages/contact.css';
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <h1>Contact</h1> <br />
-      <p>Si vous souhaitez nous contactez voici un formulaire</p> <br />
-      <form>
-        <label htmlFor="firstName">Prénom : </label>
-        <input type="text" id="firstName" name="firstName" />
-        <label htmlFor="lastName">Nom : </label>
-        <input type="text" id="lastName" name="lastName" /> <br />
-        <label htmlFor="phone">Numéro de téléphone : </label>
-        <input type="tel" id="phone" name="phone" />
-        <label htmlFor="message">Message : </label>
-        <textarea id="message" name="message" />
-        <button type="submit">Envoyer</button>
-      </form>
+    <div className="contact-container">
+      <div className="contact-image"></div> {/* L'image d'arrière-plan */}
+      <div className="contact-form">
+        <h1>Contact</h1>
+        <p>Si vous souhaitez nous contacter, veuillez utiliser ce formulaire</p>
+
+        <form>
+          <div>
+            <label htmlFor="firstName">Prénom :</label>
+            <input type="text" id="firstName" name="firstName" required />
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Nom :</label>
+            <input type="text" id="lastName" name="lastName" required />
+          </div>
+
+          <div>
+            <label htmlFor="phone">Numéro de téléphone :</label>
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required />
+          </div>
+
+          <div>
+            <label htmlFor="message">Message :</label>
+            <textarea id="message" name="message" required />
+          </div>
+
+          <button type="submit">Envoyer</button>
+        </form>
+      </div>
     </div>
   );
 };
