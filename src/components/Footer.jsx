862350f64c0ee0b5b510e0a9d3f2e79d5e6554a3
Lou Thomas <louthomas35.lt@gmail.com>
2025-03-27 12:28:14 +0100

    adding a new footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,31 @@ import '../styles/components/footer.css';
 const Footer = () => {
   return (
     <footer>
-      <nav>
-        <li>
-          <a href="/">Accueil</a>
-          <a href="/boutique">Boutique en ligne</a>
-          <a href="/contact">Contact</a>
-        </li>
-      </nav>
-      <p>Tout droit reservé</p>
-      <p>&copy; 2025 Tapis Therapy</p>
+      <div className="footer-container">
+        <div className="footer-content">
+          <div className="menu-principal">
+            <h4>MENU PRINCIPAL</h4>
+            <nav>
+              <a href="/">Accueil</a>
+              <a href="/boutique">Boutique</a>
+              <a href="/contact">Contact</a>
+              <a href="/connexion">Connexion</a>
+            </nav>
+          </div>
+          <div className="information">
+            <h4>INFORMATION :</h4>
+            <nav>
+              <a href="/conditions">Condition général d'utilisation et ventes (CGU)</a>
+              <a href="/politique-confidentialite">Politique de confidentialité</a>
+              <a href="/cookies">Politique de cookies (UE)</a>
+              <a href="/mentions-legales">Mentions Légales</a>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        © 2025 Tapis Therapy
+      </div>
     </footer>
   );
 }
