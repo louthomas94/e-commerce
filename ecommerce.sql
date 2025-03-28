7f6bc353ee08fc10d3e03aa0c2eaa15224c3062b
abdoulaye <cissokhoabdoulaye069@gmail.com>
2025-03-28 20:52:19 +0100

    modification du fichier SQL de la base de données

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 28 mars 2025 à 11:33
+-- Généré le : ven. 28 mars 2025 à 20:48
 -- Version du serveur : 8.0.39
 -- Version de PHP : 8.2.12
 
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `sku_article` varchar(20) NOT NULL,
-  `nom_article` varchar(100) NOT NULL,
+  `id_article` int NOT NULL,
+  `nom_article` varchar(150) NOT NULL,
   `description` text,
   `prix` decimal(10,2) NOT NULL,
-  `stock` int NOT NULL DEFAULT '0',
-  `id_categorie` int DEFAULT NULL
+  `stock` int NOT NULL,
+  `id_category` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -43,26 +43,9 @@ CREATE TABLE `articles` (
 --
 
 CREATE TABLE `categories` (
-  `id_categorie` int NOT NULL,
-  `nom_categorie` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `clients`
---
-
-CREATE TABLE `clients` (
-  `id_client` int NOT NULL,
-  `matricule` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `prenom` varchar(50) NOT NULL,
-  `nom` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `adresse` text NOT NULL,
-  `num_tel` varchar(20) NOT NULL
+  `id_category` int NOT NULL,
+  `nom_category` varchar(100) NOT NULL,
+  `description` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -73,10 +56,9 @@ CREATE TABLE `clients` (
 
 CREATE TABLE `commande` (
   `id_commande` int NOT NULL,
-  `id_client` int NOT NULL,
-  `date_commande` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `statut` enum('en cours','expédiée','livrée','annulée') DEFAULT 'en cours',
-  `id_code` int DEFAULT NULL
+  `date_commande` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_client` int DEFAULT NULL,
+  `statut` enum('En attente','Payée','Annulée') NOT NULL DEFAULT 'En attente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -87,8 +69,8 @@ CREATE TABLE `commande` (
 
 CREATE TABLE `commande_articles` (
   `id_commande` int NOT NULL,
-  `sku_article` varchar(20) NOT NULL,
-  `quantite` int NOT NULL DEFAULT '1'
+  `id_article` int NOT NULL,
+  `quantite` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -99,12 +81,64 @@ CREATE TABLE `commande_articles` (
 
 CREATE TABLE `paiements` (
   `id_paiement` int NOT NULL,
-  `id_commande` int NOT NULL,
-  `methode` enum('carte bancaire','paypal','virement','cryptomonnaie') NOT NULL,
-  `statut` enum('en attente','effectué','échoué') DEFAULT 'en attente',
+  `id_commande` int DEFAULT NULL,
   `montant` decimal(10,2) NOT NULL,
-  `date_paiement` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `date_paiement` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `moyen_paiement` enum('Carte bancaire','Paypal','Virement bancaire') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_client` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `passwords` varchar(255) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `adresse` text NOT NULL,
+  `num_tel` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_client`, `username`, `passwords`, `prenom`, `nom`, `email`, `adresse`, `num_tel`) VALUES
+(1, 'testuser', 'password123', 'Jean', 'Dupont', 'jean.dupont@example.com', '123 Rue Exemple', '0123456789'),
+(2, 'd', '$2y$10$c.2UbJCdtUSloElDRV0vDOeJBq5HvQesuJsmN1vnIqfxxkpUmZx8m', 'd', 'd', 'dddzd&@gmailc.omd', 'd', 'd'),
+(3, 'bgc', '$2y$10$i0ki/O5RPFpLHR.nL2evWOg6SBrdowo3Chp9767GiFx9fMzIcGIqe', 'balla', 'gueye', 'RBEBR@gmail.com', 'ergaethrgqhte', 'xx xx xx xx xm'),
+(4, 'yoooooo', '$2y$10$.QEuKUfkKbZCFtORjj0.NOIywOQ6JjnsGWpHbgZ6wrVhYS7VWfA9W', 'adyan', '94', 'sgevever@fr', 'erggrever', 'erververv'),
+(5, 'root', '', 'abdoulaye', 'cissokho', 'abdoulaye@gmail.com', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nom_user` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `nom_user`, `mdp`) VALUES
+(1, 'cissokho', 'abdoulaye', 'abdoulaye@gmail.com', 'jygliyg', '$2y$10$hdlHKq/aqJoQrCgPBo4SeOJrfZbX7wfh5IIF3ZbvWobX57B6VPDDG'),
+(3, 'cissokho', 'abdoulaye', 'abdoulaye069@gmail.com', 'jygliym', '$2y$10$ytMjMGqjZ9EWUcuCZYwipeBckNLvlKAXMg2imn9UWX.H2eduRuG2C'),
+(4, 'thomas', 'lou', 'lou@gmail.com', 'lou94', '$2y$10$djc4BuzvpqbzEbkqC0XAcuLtCy3hQCj18TD3XmDk7xFn3k88x0ydC'),
+(5, 'thomas', 'lou', 'lou49@gmail.com', 'lou49', '$2y$10$dFDMobIW7ADzabGJdt/4ruaU8nMYLfk7ICzY7NNeCYgeta1MX.PWC');
 
 --
 -- Index pour les tables déchargées
@@ -114,24 +148,14 @@ CREATE TABLE `paiements` (
 -- Index pour la table `articles`
 --
 ALTER TABLE `articles`
-  ADD PRIMARY KEY (`sku_article`),
-  ADD KEY `id_categorie` (`id_categorie`);
+  ADD PRIMARY KEY (`id_article`),
+  ADD KEY `id_category` (`id_category`);
 
 --
 -- Index pour la table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id_categorie`);
-
---
--- Index pour la table `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id_client`),
-  ADD UNIQUE KEY `matricule` (`matricule`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `num_tel` (`num_tel`);
+  ADD PRIMARY KEY (`id_category`);
 
 --
 -- Index pour la table `commande`
@@ -144,8 +168,8 @@ ALTER TABLE `commande`
 -- Index pour la table `commande_articles`
 --
 ALTER TABLE `commande_articles`
-  ADD PRIMARY KEY (`id_commande`,`sku_article`),
-  ADD KEY `sku_article` (`sku_article`);
+  ADD PRIMARY KEY (`id_commande`,`id_article`),
+  ADD KEY `id_article` (`id_article`);
 
 --
 -- Index pour la table `paiements`
@@ -155,20 +179,36 @@ ALTER TABLE `paiements`
   ADD KEY `id_commande` (`id_commande`);
 
 --
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_client`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `nom_user` (`nom_user`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id_article` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id_categorie` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `clients`
---
-ALTER TABLE `clients`
-  MODIFY `id_client` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_category` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `commande`
@@ -183,6 +223,18 @@ ALTER TABLE `paiements`
   MODIFY `id_paiement` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_client` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- Contraintes pour les tables déchargées
 --
 
@@ -190,14 +242,20 @@ ALTER TABLE `paiements`
 -- Contraintes pour la table `articles`
 --
 ALTER TABLE `articles`
-  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`) ON DELETE SET NULL;
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`) ON DELETE SET NULL;
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `users` (`id_client`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `commande_articles`
 --
 ALTER TABLE `commande_articles`
   ADD CONSTRAINT `commande_articles_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`) ON DELETE CASCADE,
-  ADD CONSTRAINT `commande_articles_ibfk_2` FOREIGN KEY (`sku_article`) REFERENCES `articles` (`sku_article`) ON DELETE CASCADE;
+  ADD CONSTRAINT `commande_articles_ibfk_2` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id_article`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `paiements`
